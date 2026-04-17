6673895fa15400d3cfea2b6f5914868505e03dfa
orun-sunny <sunny.mukut@gmail.com>
2026-04-17 11:49:05 +0600

    fix/route-issue-fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const searchRoutes = require('./routes/searchRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
@@ -19,6 +19,7 @@ app.use(express.json());
 
 // Basic Route for testing
 app.get('/', (req, res) => {
+    console.log('Root route accessed');
     res.json({ message: 'Welcome to the Quran API' });
 });
 
